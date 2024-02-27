c064d01fa212f9a91a26dc1bae7b2b760fe1d89c
Cristóvão Sampaio <cristovaogmsampaio0@gmail.com>
2024-02-27 23:38:01 +0000

    Used Tweens to add animation to the Slots when selecting a Fruit.

--- SlotMachine/Assets/Scripts/Slot.cs
+++ SlotMachine/Assets/Scripts/Slot.cs
@@ -15,13 +15,22 @@ public class Slot : MonoBehaviour
 
     [SerializeField, Range(0.0f, 1.0f)] private float _timeBetweenChange = 0.02f;
 
+    [Header("Tween Configurations")]
+    [SerializeField] private LeanTweenType _tweenType;
+    [SerializeField] private float _tweenTime = 0.1f;
+    [SerializeField] private float _tweenMoveAmount = -10f;
+
+    private int _tweenId;
+
     public void Spin(float animationTime)
     {
+        _tweenId = LeanTween.moveLocalY(gameObject, _tweenMoveAmount, _tweenTime).setLoopPingPong().setEase(_tweenType).uniqueId;
         StartCoroutine(AnimateSpin(animationTime));
     }
 
     private IEnumerator AnimateSpin(float animationTime)
     {
+        bool isDoingEndTween = false;
         float timeElapsed = 0.0f;
         FruitSO selectedFruit = _fruitSOs[Random.Range(0, _fruitSOs.Length)];
 
@@ -34,9 +43,27 @@ public class Slot : MonoBehaviour
 
             timeElapsed += Time.deltaTime + _timeBetweenChange;
 
+            if (!isDoingEndTween && (animationTime - timeElapsed) < .2f)
+            {
+                isDoingEndTween = true;
+                EndTween();
+            }
+
             yield return new WaitForSeconds(_timeBetweenChange);
         }
 
         OnSpinComplete?.Invoke(selectedFruit);
+    }
+
+    private void EndTween()
+    {
+        LeanTween.cancel(_tweenId);
+
+        LeanTween.moveLocalY(gameObject, 40f, .2f).setEaseInCirc().setOnComplete(MoveBackTween);
+    }
+
+    private void MoveBackTween()
+    {
+        LeanTween.moveLocalY(gameObject, 0f, .5f).setEaseOutElastic();
     }
 }

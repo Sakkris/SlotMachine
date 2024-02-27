764fb7a93d485edb991c91169d5ef4c1f5135630
Cristóvão Sampaio <cristovaogmsampaio0@gmail.com>
2024-02-27 22:19:03 +0000

    Created a base behavior for the Slot Machine Manager.

--- SlotMachine/Assets/Scripts/Slot.cs
+++ SlotMachine/Assets/Scripts/Slot.cs
@@ -1,20 +1,19 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEngine.Rendering.DebugUI;
+using Random = UnityEngine.Random;
 
 public class Slot : MonoBehaviour
 {
-    [SerializeField] private FruitSO[] _fruitSprites;
+    public Action<FruitSO> OnSpinComplete;
+
+    [SerializeField] private FruitSO[] _fruitSOs;
 
     [SerializeField] private Image _imageContainer;
 
     [SerializeField, Range(0.0f, 1.0f)] private float _timeBetweenChange = 0.02f;
-
-    [Header("Bet")]
-    [SerializeField] private TextMeshProUGUI _betText;
 
     public void Spin(float animationTime)
     {
@@ -24,21 +23,20 @@ public class Slot : MonoBehaviour
     private IEnumerator AnimateSpin(float animationTime)
     {
         float timeElapsed = 0.0f;
+        FruitSO selectedFruit = _fruitSOs[Random.Range(0, _fruitSOs.Length)];
 
         while (timeElapsed < animationTime)
         {
-            int randomSpriteIdx = Random.Range(0, _fruitSprites.Length);
+            int randomFruitSOIdx = Random.Range(0, _fruitSOs.Length);
 
-            _imageContainer.sprite = _fruitSprites[randomSpriteIdx].sprite;
+            selectedFruit = _fruitSOs[randomFruitSOIdx];
+            _imageContainer.sprite = selectedFruit.sprite;
 
             timeElapsed += Time.deltaTime + _timeBetweenChange;
 
             yield return new WaitForSeconds(_timeBetweenChange);
         }
-    }
 
-    public void UpdateBetAmount(int betAmount)
-    {
-        _betText.text = string.Format("Bet:\n{0}", betAmount.ToString());
+        OnSpinComplete?.Invoke(selectedFruit);
     }
 }
--- SlotMachine/Assets/Scripts/SlotMachineManager.cs
+++ SlotMachine/Assets/Scripts/SlotMachineManager.cs
@@ -12,14 +12,16 @@ public class SlotMachineManager : MonoBehaviour
     [SerializeField, Range(0.0f, 5.0f)] private float _animationTime = 1.0f;
     [SerializeField, Range(0.0f, 5.0f)] private float _animationDiffTime = .2f;
 
-    [Header("Gambling")]
-    [SerializeField, Range(0.0f, 5.0f)] private int _playerCredits;
-    [SerializeField, Range(0.0f, 5.0f)] private int _betAmount;
-
     [Header("Interface")]
     [SerializeField] private TextMeshProUGUI _creditsText;
     [SerializeField] private TextMeshProUGUI _betText;
 
+    private int _playerCredits;
+    private int _betAmount;
+
+    private List<FruitSO> _selectedFruitsList;
+    private int _completedSlots;
+    private bool _isSpinning;
 
     private void Awake()
     {
@@ -28,11 +30,42 @@ public class SlotMachineManager : MonoBehaviour
             Instance = this;
         }
 
+        _playerCredits = 0;
+        _betAmount = 5;
+        _isSpinning = false;
+
         UpdateBetText();
+    }
+
+    private void Start()
+    {
+        foreach (Slot slot in _slots)
+        {
+            slot.OnSpinComplete += SlotSpinCompleted;
+        }
+
+        _selectedFruitsList = new List<FruitSO>();
+    }
+
+    private void OnDisable()
+    {
+        foreach (Slot slot in _slots)
+        {
+            slot.OnSpinComplete -= SlotSpinCompleted;
+        }
     }
 
     public void SpinAll()
     {
+        if (_isSpinning || _playerCredits < _betAmount)
+            return;
+
+        _playerCredits -= _betAmount;
+        UpdateCreditsText();
+
+        _completedSlots = 0;
+        _isSpinning = true;
+
         float slotAnimationTime = _animationTime;
 
         foreach (Slot slot in _slots)
@@ -43,9 +76,37 @@ public class SlotMachineManager : MonoBehaviour
         }
     }
 
+    private void SlotSpinCompleted(FruitSO fruitSO)
+    {
+        if (_selectedFruitsList == null)
+        {
+            _selectedFruitsList = new List<FruitSO> { fruitSO };
+        }
+        else
+        {
+            _selectedFruitsList.Add(fruitSO);
+        }
+
+        _completedSlots++;
+
+        if (_completedSlots == _slots.Length)
+        {
+            _isSpinning = false;
+            CalculateWin();
+        }
+
+    }
+
+    private void CalculateWin()
+    {
+        Debug.Log("Here");
+        _selectedFruitsList.Clear();
+    }
 
     public void ChangeBetAmount(int bet)
     {
+        if (_isSpinning) return;
+
         _betAmount = bet;
         UpdateBetText();
     }
